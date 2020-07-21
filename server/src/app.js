dca586f19556a895edaca19abd241f739837b797
nattanonAB <tape.nattanon@hmail.com>
2020-07-21 22:12:03 +0700

    floder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,44 +1,54 @@
 let express = require('express')
 const app = express()
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
-app.use(bodyParser.json())
+ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/status', function (req, res){
+ require('./routes')(app)
+
+ let port = process.env.PORT || config.port
+sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
+})
+
+ app.get('/status', function (req, res){
     res.send('Hello nodejs server')
 })
 
-app.get('/hello/:person', function (req,res) {
+ app.get('/hello/:person', function (req,res) {
     console.log('hello - ' + req.params.person)
     res.send('sey hello with ' + req.params.person)
 })
 
-// get user by id
-    app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน ' + req.params.userId)
-})
+ // get user by id
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน: '+ req.params.userId)
+    })
 // get all user
-    app.get('/users', function (req, res) {
+app.get('/users', function (req, res) {
     res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
 })
-// create user
+
+ // create user
 app.post('/user/', function (req, res) {
     res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
 })
 // edit user
-    app.put('/user/:userId', function (req, res) {
-    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + ' : ' +
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผุ้ใช้งาน: ' + req.params.userId + ' : ' +
     JSON.stringify(req.body))
 })
 // delete user
-    app.delete('/user/:userId', function (req, res) {
+app.delete('/user/:userId', function (req, res) {
     res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
     JSON.stringify(req.body))
 })
-
-let port = 8081
-
-app.listen(port, function(){
-    console.log('server running on ' + port)
+// get lastuser
+app.get('/lastuser', function (req, res) {
+    res.send('ID สุดท้าย: ' + JSON.stringify(req.body))
 })
